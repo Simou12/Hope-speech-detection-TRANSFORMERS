10d2f184095a8733322507cdbd6f570783b19966
Simou12 <117384981+Simou12@users.noreply.github.com>
2023-06-09 17:05:08 +0200

    Update tweetExtract.py

--- tweetExtract.py
+++ tweetExtract.py
@@ -30,8 +30,6 @@ access_token_secret = 'dKMDm9btO3YDF735DB7Jez18xDV8EFi7c3NnpMG8BO7yv'
 date = datetime.datetime.now().date()
 date = pd.to_datetime(date)
 
-keyword = "drone commercial OR drones commerciaux OR capacité drone OR sécurité drone"
-
 
 def get_bearer_header():
     uri_token_endpoint = 'https://api.twitter.com/oauth2/token'
@@ -60,94 +58,134 @@ def get_bearer_header():
 
 
 def getConversationId(id):
-    url = f'https://api.twitter.com/2/tweets/{id}'
-    headers = {
-        'Authorization': 'Bearer YOUR_BEARER_TOKEN',
-        'Content-Type': 'application/json'
-    }
+    uri = 'https://api.twitter.com/2/tweets?'
+
     params = {
+        'ids': id,
         'tweet.fields': 'conversation_id'
     }
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        conversation_id = data['data']['conversation_id']
-        return conversation_id
+
+    bearer_header = get_bearer_header()
+    resp = requests.get(uri, headers=bearer_header, params=params)
+    json_data = resp.json()
+    if 'data' in json_data:
+        return json_data['data'][0]['conversation_id']
     else:
         return None
 
 
-# Authenticate with Twitter API
+def getConversation(tweet_id):
+    conversation = []
+
+    # Get the conversation responses
+    replies = tweepy.Cursor(api.search_tweets, q=f"to:{tweet_id}", tweet_mode='extended').items()
+
+    # Add the responses to the list
+    for reply in replies:
+        if reply.in_reply_to_status_id_str == tweet_id:
+            conversation.append(reply)
+
+    # Check if the tweet has a conversation
+    if conversation:
+        # Get the tweet initial
+        tweet = api.get_status(tweet_id, tweet_mode='extended')
+        conversation.insert(0, tweet)
+
+    return conversation
+
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 limit = 300
-tweets = tweepy.Cursor(api.search_tweets, q=keyword, count=10, tweet_mode='extended', lang="fr").items(limit)
-if auth.access_token is None:
-    print("Authentication failed.")
-else:
-    print("Authentication successful.")
-    tweetsDf = pd.DataFrame(
-        columns=
-        ["id", "conversation_id", "created_at", "date", "time", "timezone", "user_id", "username", "name", "place",
-         "tweet", "language", "mentions", "urls", "photos", "replies_count", "retweets_count", "likes_count",
-         "hashtags", "cashtags", "link", "retweet", "quote_url", "video", "thumbnail", "near", "geo", "source",
-         "user_rt_id", "user_rt", "retweet_id", "reply_to", "retweet_date", "translate", "trans_src", "trans_dest"]
-    )
-    i = 0
-    for tweet in tweets:
-        tweetData = {
-            "id": tweet.id,
-            "conversation_id": getConversationId(tweet.id),
-            "created_at": tweet.created_at,
-            "date": tweet.created_at.date(),
-            "time": tweet.created_at.time(),
-            "timezone": tweet.created_at.strftime("%Z"),
-            "user_id": tweet.user.id,
-            "username": tweet.user.screen_name,
-            "name": tweet.user.name,
-            "place": tweet.place.full_name if tweet.place else None,
-            "tweet": tweet.full_text,
-            "language": tweet.lang,
-            "mentions": [mention["screen_name"] for mention in tweet.entities["user_mentions"]],
-            "urls": [url["expanded_url"] for url in tweet.entities["urls"]],
-            "photos": [media["media_url_https"] for media in tweet.entities.get("media", []) if
-                       media["type"] == "photo"],
-            # "replies_count": tweet.reply_count,
-            "retweets_count": tweet.retweet_count,
-            "likes_count": tweet.favorite_count,
-            "hashtags": [hashtag["text"] for hashtag in tweet.entities["hashtags"]],
-            "cashtags": [cashtag["text"] for cashtag in tweet.entities["symbols"]],
-            "link": f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}",
-            "retweet": tweet.retweeted,
-            # "quote_url": tweet.quoted_status_permalink["expanded"],
-            "video": tweet.entities.get("media", [{}])[0].get("video_info", {}).get("variants", [{}])[0].get("url"),
-            "thumbnail": tweet.entities.get("media", [{}])[0].get("media_url_https"),
-            "near": tweet.geo["coordinates"] if tweet.geo else None,
-            "geo": tweet.place.bounding_box.coordinates if tweet.place else None,
-            "source": tweet.source,
-            "user_rt_id": tweet.user.id_str,
-            "user_rt": tweet.user.screen_name,
-            "retweet_id": tweet.retweeted_status.id_str if hasattr(tweet, "retweeted_status") else None,
-            "reply_to": tweet.in_reply_to_status_id_str,
-            "retweet_date": tweet.retweeted_status.created_at.strftime("%Y-%m-%d %H:%M:%S")
-            if hasattr(tweet, "retweeted_status")
-            else None,
-            #   "translate" = translate_client.translate_text(
-            #   request={
-            #       "parent": parent,
-            #       "contents": [tweet.full_text],
-            #       "source_language":srcLanguage,
-            #       "target_language_code": targetLanguage,
-            #   }
-            # ).translations[0].translated_text
 
-        }
-        tweetsDf = tweetsDf.append(tweetData, ignore_index=True)
-        time.sleep(3)
-    # save the tweets in a csv file
-    tweetsDf.to_csv("tweets.csv", index=True)
+keywords = ["drone commercial", "capacité drone", "sécurité drone"]
+i = 0
+for keyword in keywords:
+    tweets = tweepy.Cursor(api.search_tweets, q=keyword, count=10, tweet_mode='extended', lang="fr").items(limit)
+    if auth.access_token is None:
+        print("Authentication failed.")
+    else:
+        print("Authentication successful.")
+        tweetsDf = pd.DataFrame(
+            columns=
+            ["id", "conversation_id", "created_at", "date", "time", "timezone", "user_id", "username", "name", "place",
+             "tweet", "language", "mentions", "urls", "photos", "replies_count", "retweets_count", "likes_count",
+             "hashtags", "cashtags", "link", "retweet", "quote_url", "video", "thumbnail", "near", "geo", "source",
+             "user_rt_id", "user_rt", "retweet_id", "reply_to", "retweet_date", "translate", "trans_src", "trans_dest"]
+        )
+        j = 0
+        for tweet in tweets:
+            convId = getConversationId(tweet.id)
+            tweetData = {
+                "id": tweet.id,
+                "conversation_id": convId,
+                "created_at": tweet.created_at,
+                "date": tweet.created_at.date(),
+                "time": tweet.created_at.time(),
+                "timezone": tweet.created_at.strftime("%Z"),
+                "user_id": tweet.user.id,
+                "username": tweet.user.screen_name,
+                "name": tweet.user.name,
+                "place": tweet.place.full_name if tweet.place else None,
+                "tweet": tweet.full_text,
+                "language": tweet.lang,
+                "mentions": [mention["screen_name"] for mention in tweet.entities["user_mentions"]],
+                "urls": [url["expanded_url"] for url in tweet.entities["urls"]],
+                "photos": [media["media_url_https"] for media in tweet.entities.get("media", []) if
+                           media["type"] == "photo"],
+                # "replies_count": tweet.reply_count,
+                "retweets_count": tweet.retweet_count,
+                "likes_count": tweet.favorite_count,
+                "hashtags": [hashtag["text"] for hashtag in tweet.entities["hashtags"]],
+                "cashtags": [cashtag["text"] for cashtag in tweet.entities["symbols"]],
+                "link": f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}",
+                "retweet": tweet.retweeted,
+                # "quote_url": tweet.quoted_status_permalink["expanded"],
+                "video": tweet.entities.get("media", [{}])[0].get("video_info", {}).get("variants", [{}])[0].get("url"),
+                "thumbnail": tweet.entities.get("media", [{}])[0].get("media_url_https"),
+                "near": tweet.geo["coordinates"] if tweet.geo else None,
+                "geo": tweet.place.bounding_box.coordinates if tweet.place else None,
+                "source": tweet.source,
+                "user_rt_id": tweet.user.id_str,
+                "user_rt": tweet.user.screen_name,
+                "retweet_id": tweet.retweeted_status.id_str if hasattr(tweet, "retweeted_status") else None,
+                "reply_to": tweet.in_reply_to_status_id_str,
+                "retweet_date": tweet.retweeted_status.created_at.strftime("%Y-%m-%d %H:%M:%S")
+                if hasattr(tweet, "retweeted_status")
+                else None,
+                #   "translate" = translate_client.translate_text(
+                #   request={
+                #       "parent": parent,
+                #       "contents": [tweet.full_text],
+                #       "source_language":srcLanguage,
+                #       "target_language_code": targetLanguage,
+                #   }
+                # ).translations[0].translated_text
+
+            }
+            tweetsDf = tweetsDf.append(tweetData, ignore_index=True)
+
+            tweets_df = pd.DataFrame()  # Dataframe pour stocker les tweets
+            conversations_df = pd.DataFrame(columns=["tweet_id", "tweet"])
+            conversation = getConversation(tweet.id)
+            if conversation is not None and len(conversation) > 0:
+                # Parcourir les tweets de la conversation
+                for conv_tweet in conversation:
+                    # Ajouter le tweet à la ligne du dataframe de la conversation
+                    conversation_data = {
+                        "conversation_id": convId,
+                        "tweet_id": conv_tweet.id,
+                        "tweet": conv_tweet.full_text
+                    }
+                    conversations_df = conversations_df.append(conversation_data, ignore_index=True)
+                conversations_df.to_csv("tweets" + i + "conversations" + j + ".csv")
+                i += 1
+
+            time.sleep(3)
+        # save the tweets in a csv file
+        tweetsDf.to_csv("tweetsExtracted" + str(i) + ".csv", index=True)
+        tweetsDf.astype(str)
 
 
 
